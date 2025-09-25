9aaeedee0ee5b65b9cc09558c2b825f4949e2642
cyokozai <yuu4suke27@outlook.jp>
2025-09-25 18:59:11 +0900

    EKS設定を更新し、アクセスエントリとポリシーの関連付けを追加。VPC設定を拡張し、サブネットを追加。

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -18,6 +18,22 @@ module "eks" {
     }
   }
 
+  access_entries = {
+    self_admin = {
+      principal_arn = var.eks_access_entry_principal_arn
+      type          = "STANDARD"
+
+      policy_associations = {
+        cluster_admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
   tags = var.common_tags
 
   cluster_endpoint_public_access  = true
@@ -31,28 +47,13 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-northeast-1a"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["ap-northeast-1a", "ap-northeast-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
   tags = var.common_tags
-}
-
-resource "aws_eks_access_entry" "access_entry" {
-  cluster_name      = module.eks.cluster_name
-  principal_arn     = var.eks_access_entry_principal_arn
-  type              = "STANDARD"
-}
-
-resource "aws_eks_access_policy_association" "admin_policy_association" {
-  cluster_name = module.eks.cluster_name
-  principal_arn = var.eks_access_entry_principal_arn
-  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  access_scope {
-    type = "cluster"
-  }
 }
