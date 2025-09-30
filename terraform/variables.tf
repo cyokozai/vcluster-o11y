5129c7687131976aa42d2074e7650590f0352e52
cyokozai <yuu4suke27@outlook.jp>
2025-09-30 20:23:35 +0900

    vcluster設定を更新し、永続ストレージの設定を追加。不要なプロバイダー設定を削除。

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,21 +29,3 @@ variable "eks_access_entry_principal_arn" {
   type = string
   description = "EKS Access Entry principal ARN"
 }
-
-variable "vcluster_namespace" {
-  type        = string
-  default     = "vcluster-demo"
-  description = "Namespace to install vcluster"
-}
-
-variable "vcluster_chart_version" {
-  type        = string
-  default     = null
-  description = "Version of the vcluster Helm chart"
-}
-
-variable "vcluster_values_file" {
-  type        = string
-  default     = "manifests/vcluster/vcluster.yaml"
-  description = "Path to vcluster values file"
-}
