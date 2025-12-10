20bb302d7f962fc58c50b0b8cee776a83adbce92
cyokozai <yuu4suke27@outlook.jp>
2025-12-11 03:37:10 +0900

    node 2 -> 4

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,8 +11,8 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size  = 2
-      max_size      = 3
+      desired_size  = 4
+      max_size      = 4
       min_size      = 1
       instance_types = ["t3.medium"]
     }
