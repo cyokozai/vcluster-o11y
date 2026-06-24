d9a0cac4c6e867f1130a444d2a87a4308a0bb883
cyokozai <yuu4suke27@outlook.jp>
2026-06-24 14:57:22 +0900

    ECRを追加

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,13 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+
+output "ecr_repository_url" {
+  value       = aws_ecr_repository.go_api_server.repository_url
+  description = "ECR repository URL for go-api-server (use this in manifests/pattern-*/deploy.yaml)"
+}
+
+output "aws_account_id" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "AWS account ID"
+}
